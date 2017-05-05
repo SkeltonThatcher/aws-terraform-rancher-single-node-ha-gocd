27c3784c3f0a73810e26858085bbc0a2376b0538
Rich Bos <richard.bosomworth@skeltonthatcher.com>
2017-05-05 10:34:05 +0100

    First commit

---
+++ iam.tf
@@ -0,0 +1,59 @@
+## IAM EC2 policy + role + profile
+
+# IAM role policy
+
+resource "aws_iam_role_policy" "rancher" {
+  name = "rancher"
+  role = "${aws_iam_role.rancher.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect":"Allow",
+        "Action":[
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "autoscaling:*",
+          "route53:*",
+          "s3:*",
+          "sns:*",
+          "cloudwatch:*"
+        ],
+        "Resource":"*"
+    }
+  ]
+}
+EOF
+}
+
+# IAM role
+
+resource "aws_iam_role" "rancher" {
+  name = "rancher"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# IAM instance profile
+
+resource "aws_iam_instance_profile" "rancher" {
+  name  = "rancher"
+  roles = ["${aws_iam_role.rancher.name}"]
+}
